bec8e20ba70064a37843f66d6782068489187623
lallandon-max <krishnamuraripandit9608@gmail.com>
2025-02-11 18:09:35 +0530

    Update GeminiReplyGenerator.java

--- app/src/main/java/zo/ro/whatsappreplybot/apis/GeminiReplyGenerator.java
+++ app/src/main/java/zo/ro/whatsappreplybot/apis/GeminiReplyGenerator.java
@@ -37,8 +37,8 @@ public class GeminiReplyGenerator {
         API_KEY = sharedPreferences.getString("api_key", "not-set").trim();
         LLM_MODEL = sharedPreferences.getString("llm_model", "gemini-1.5-flash");
         defaultReplyMessage = sharedPreferences.getString("default_reply_message", context.getString(R.string.default_bot_message));
-        aiReplyLanguage = sharedPreferences.getString("ai_reply_language", "English");
-        botName = sharedPreferences.getString("bot_name", "Yuji");
+        aiReplyLanguage = sharedPreferences.getString("ai_reply_language", "not-set");
+        botName = sharedPreferences.getString("bot_name", "not-set");
     }
 
     public void generateReply(String sender, String message, CustomReplyGenerator.OnReplyGeneratedListener listener) {
@@ -88,7 +88,10 @@ public class GeminiReplyGenerator {
             prompt.append(" Generate only reply texts. Don't add any other texts.");
             prompt.append(" Always reply in ").append(aiReplyLanguage).append(".");
             prompt.append(" Reply within 1 or 2 sentences whenever possible.");
-            prompt.append(" Be polite, context-aware, and ensure your replies are relevant to the conversation.");
+            prompt.append(" Be polite, context-aware ");
+            prompt.append(" Reply in gen Z tone ");
+            prompt.append(" Stay natural and casual, making sure the sender never suspects they’re talking to a bot.");  
+            prompt.append(" reply should be precise do not stick with topic rather try to get in flow with senders messages."); 
             prompt.append("\n\n\nMost recent message (from ");
             prompt.append(sender).append("): ");
             prompt.append(message);
@@ -99,7 +102,11 @@ public class GeminiReplyGenerator {
         prompt.append("You are a WhatsApp auto-reply bot named ").append(botName);
         prompt.append("Your task is replying to the incoming message. ");
         prompt.append("Always reply in ").append(aiReplyLanguage);
-        prompt.append(". Be polite, context-aware, and ensure your replies are relevant to the conversation.\n\n");
+        prompt.append(" Be polite, context-aware ");
+        prompt.append(" Reply in gen Z tone ");
+        prompt.append(" Stay natural and casual, making sure the sender never suspects they’re talking to a bot.");  
+        prompt.append(" reply should be precise do not stick with topic rather try to get in flow with senders messages.");    
+        prompt.append(". Be polite, context-aware.\n\n");
         prompt.append("\n\n\nIncoming message (from ");
         prompt.append(sender).append("): ");
         prompt.append(message);
